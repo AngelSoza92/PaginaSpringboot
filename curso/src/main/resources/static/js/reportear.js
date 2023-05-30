f5600453ff9cc1b551576ebdbebb582f988d8757
Angel Martin Soza Sepúlveda <114920201+AngelSoza92@users.noreply.github.com>
2023-05-30 15:13:09 -0400

    charts
    dashboard redirecciona a nuevo dash

--- curso/src/main/resources/static/js/reportear.js
+++ curso/src/main/resources/static/js/reportear.js
@@ -9,7 +9,7 @@ function reportear(tipo){
   } else if (tipo === 'stock') {
     argumento = "http://10.107.226.241:8050/stock100/";
   } else if (tipo === 'traspasos') {
-    argumento = "http://10.107.226.241:8050/traspasos/";
+    argumento = "http://10.107.226.241:8050/traspasos100/";
   } else if (tipo === 'tracking') {
     argumento = "http://10.107.226.241:8050/tracking100/";
   } else if (tipo === 'pkt') {
@@ -19,29 +19,31 @@ function reportear(tipo){
   } else if (tipo === 'capacidades') {
     argumento = "http://10.107.226.241:8050/capacidades/";
   } else if (tipo === 'charts12') {
-    argumento = "http://10.107.226.241:8051/clu/";
+    argumento = "http://10.107.226.241:8050/clu12/";
   } else if (tipo === 'antiguedad12') {
-    argumento = "http://10.107.226.241:8051/antiguedad/";
+    argumento = "http://10.107.226.241:8050/antiguedad12/";
   } else if (tipo === 'stock12') {
-    argumento = "http://10.107.226.241:8051/stock/";
+    argumento = "http://10.107.226.241:8050/stock12/";
   } else if (tipo === 'traspasos12') {
-    argumento = "http://10.107.226.241:8051/traspasos/";
+    argumento = "http://10.107.226.241:8050/traspasos12/";
   } else if (tipo === 'tracking12') {
-    argumento = "http://10.107.226.241:8051/tracking/";
+    argumento = "http://10.107.226.241:8050/tracking12/";
   } else if (tipo === 'pkt12') {
-    argumento = "http://10.107.226.241:8051/pkt/";
+    argumento = "http://10.107.226.241:8050/pkt12/";
   } else if (tipo === 'charts150') {
-    argumento = "http://10.107.226.241:81/clu/";
+    argumento = "http://10.107.226.241:8050/clu150/";
   } else if (tipo === 'antiguedad150') {
-    argumento = "http://10.107.226.241:81/antiguedad/";
+    argumento = "http://10.107.226.241:8050/antiguedad150/";
   } else if (tipo === 'stock150') {
-    argumento = "http://10.107.226.241:81/stock/";
+    argumento = "http://10.107.226.241:8050/stock150/";
   } else if (tipo === 'traspasos150') {
-    argumento = "http://10.107.226.241:81/traspasos/";
+    argumento = "http://10.107.226.241:8050/traspasos150/";
   } else if (tipo === 'tracking150') {
-    argumento = "http://10.107.226.241:81/tracking/";
+    argumento = "http://10.107.226.241:8050/tracking150/";
   } else if (tipo === 'pkt150') {
-    argumento = "http://10.107.226.241:81/pkt/";
+    argumento = "http://10.107.226.241:8050/pkt150/";
+  } else if (tipo === 'despachosbodegas') {
+    argumento = "http://10.107.226.241:8050/despachosbodegas/";
   }
 
   // Guardar el src en localStorage
